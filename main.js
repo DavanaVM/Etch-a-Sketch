188acb9df666d9cb5a01654d0a45ca5c71e1c59e
DavanaVM <gotfriddavid@gmx.de>
2023-01-31 14:28:23 +0100

    Finished

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 const main = document.querySelector("#main");
+size = 900
+sizemult = 30;
+const colors = document.querySelector("#colors")
+const reset = document.querySelector("#clear")
 
-for(let i = 0; i < 256; i++){
+reset.addEventListener('click', () =>{
+    els = document.getElementsByClassName("sdiv")
+    Array.from(els).forEach((e) => {
+        e.setAttribute("style", "color:white")
+    });
+})
+
+const resize = document.querySelector("#resize")
+resize.addEventListener("click", () =>{
+    sizemult = prompt("enter cubes per row (max 40)")
+    if(isNaN(size) || sizemult > 40){
+        size = 256
+        sizemult = 16
+    }else{
+        size = sizemult * sizemult
+    }
+    main.innerHTML = ""
+    for(let i = 0; i < size; i++){
+        let div = document.createElement("div");
+        div.classList.toggle("sdiv");
+        if(i % sizemult == 0 && i != 0){
+            let brk = document.createElement("div")
+            brk.classList.toggle("break")
+            main.appendChild(brk)
+        }
+        div.id = "draw" +[i]
+        main.appendChild(div);
+    }
+})
+
+for(let i = 0; i < size; i++){
     let div = document.createElement("div");
     div.classList.toggle("sdiv");
-    if(i % 16 == 0 && i != 0){
+    if(i % sizemult == 0 && i != 0){
         let brk = document.createElement("div")
         brk.classList.toggle("break")
         main.appendChild(brk)
@@ -14,7 +48,7 @@ for(let i = 0; i < 256; i++){
 let mousedown = false;
 document.addEventListener('mousedown', (e) => {
     if(e.target.id.includes("draw")){
-        e.target.classList.toggle("black")
+        e.target.setAttribute("style", `background-color:${colors.value}`)
     }
     mousedown = true;
 })
@@ -24,9 +58,14 @@ document.addEventListener('mouseup', () =>{
 })
 
 
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault()
+})
+
 document.addEventListener("mouseover", (e) => {
     if(mousedown === true && e.target.id.includes("draw")){
-        e.target.classList.toggle("black")
+        //e.target.classList.toggle("black")
+        e.target.setAttribute("style", `background-color:${colors.value}`)
     }
 })
 
